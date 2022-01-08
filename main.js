61f34dd52afec690de42c089c07a47dfe507adf1
Deny Flores <deny.flores03@gmail.com>
2022-01-08 14:00:17 -0600

    Se agregan nuevos archivos

---
+++ main.js
@@ -0,0 +1,69 @@
+
+const dineroGastado = parseInt(prompt('Ingresa el monto gastado: '));
+if(dineroGastado<100){
+    console.log('Entro en la condicion');
+
+}
+else if(dineroGastado>=100 && dineroGastado <200){
+    console.log('Entro en la segunda condicion');
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+let n1 = 8;
+let n2 = 9;
+
+let suma = n1 + n1;
+let resta = n1 - n2;
+let multiplicacion = n1 * n2;
+
+function imprimirTexto(){
+    document.write("Este es un mensaje");
+}
+//imprimirTexto()
+
+function imprimirTuTexto(texto){
+    document.write("este es tu texto " + texto);
+
+
+}
+let miMensaje = "Me gustan los gatos";
+imprimirTuTexto(miMensaje);
+
+
+*/
+
